ddefa0f9b3f90dac6cea9b1026833d6e36bb771f
Brian Cappello <briancappello@gmail.com>
2018-04-05 14:45:33 -0400

    update setup.py install_requires

--- setup.py
+++ setup.py
@@ -3,21 +3,10 @@ import os
 from codecs import open
 from setuptools import setup, find_packages
 
-
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
-
 
 with open(os.path.join(ROOT_DIR, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-
-
-def read_requirements(filename):
-    def is_pkg(line):
-        return line and not line.startswith(('--', 'git', '#'))
-
-    with open(os.path.join(ROOT_DIR, filename), encoding='utf-8') as f:
-        return [line for line in f.read().splitlines() if is_pkg(line)]
-
 
 setup(
     name='Py YAML Fixtures',
@@ -38,7 +27,12 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(include=['py_yaml_fixtures']),
-    install_requires=read_requirements('requirements.txt'),
+    install_requires=[
+        'faker>=0.8.7',
+        'jinja2>=2.10',
+        'python-dateutil>=2.6.1',
+        'PyYAML>=3.12',
+    ],
     python_requires='>=3.5',
     include_package_data=True,
     zip_safe=False,
